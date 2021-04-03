7952d15c81873da3e92805939a140c9265281bf2
Nangman <nnagman@gmail.com>
2021-04-03 14:58:29 +0900

    feat: rabbimq 추가

---
+++ src/main/java/class101/foo/io/Producer.java
@@ -0,0 +1,19 @@
+package class101.foo.io;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Producer {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public Producer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendTo(String message) {
+        this.rabbitTemplate.convertAndSend("CREATE_POST_QUEUE", message);
+    }
+
+}
