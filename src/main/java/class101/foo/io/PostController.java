ae2d53ad6be315b0fe91066a32607152b2ed94ae
Nangman <nnagman@gmail.com>
2021-04-09 15:04:48 +0900

    feat: db에서 es를 사용하도록 변경

--- src/main/java/class101/foo/io/PostController.java
+++ src/main/java/class101/foo/io/PostController.java
@@ -12,18 +12,14 @@ import java.util.List;
 @RestController
 public class PostController {
 
-    private static Integer PAGE_SIZE = 20;
-
     private final PostRepository postRepository;
     private final Producer producer;
     private final ObjectMapper objectMapper;
-    private final PostCacheService postCacheService;
 
-    public PostController(PostRepository postRepository, Producer producer, ObjectMapper objectMapper, PostCacheService postCacheService) {
+    public PostController(PostRepository postRepository, Producer producer, ObjectMapper objectMapper) {
         this.postRepository = postRepository;
         this.producer = producer;
         this.objectMapper = objectMapper;
-        this.postCacheService = postCacheService;
     }
 
     // 1. 글을 작성한다.
@@ -34,27 +30,9 @@ public class PostController {
         return post;
     }
 
-    // 2. 글 목록을 페이징하여 반환
-    @GetMapping("/posts")
-    public Page<Post> getPostList(@RequestParam(defaultValue = "1") Integer page) {
-        if (page.equals(1)) {
-            return postCacheService.getFirstPostPage();
-        } else {
-            return postRepository.findAll(
-                    PageRequest.of(page - 1, PAGE_SIZE, Sort.by("id").descending())
-            );
-        }
-    }
-
-    // 3. 글 번호로 조회
-    @GetMapping("/post/{id}")
-    public Post getPostById(@PathVariable("id") Long id) {
-        return postRepository.findById(id).get();
-    }
-
     // 4. 글 내용으로 검색 -> 해당 내용이 포함된 모든 글
     @GetMapping("/search")
     public List<Post> findPostByContent(@RequestParam String content) {
-        return postRepository.findByContentContains(content);
+        return postRepository.findByContent(content);
     }
 }
